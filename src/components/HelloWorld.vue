631761a2efe3bbaf9f9e71b1b1efbc8d144f4283
DevinWain <1065053260@qq.com>
2020-10-30 18:20:51 +0800

    completed the marjor layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,157 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <header class="header">
+      <el-row class="row-header" type="flex" justify="center">
+        <el-col :span="15">
+          <h2 class="header-h2">{{ pj_name }}</h2>
+        </el-col>
+      </el-row>
+    </header>
+
+    <div class="content">
+      <el-row
+        style="height: 280px"
+        type="flex"
+        justify="center"
+      >
+        <el-col :span="20" id="first">
+          <br />
+          <h1 style="font-size: 50px">Hello,{{ welcome }},...</h1>
+          <p>
+            This is a template for a simple marketing or informational website.
+            It includes a large callout called a jumbotron and three supporting
+            pieces of content. Use it as a starting point to create something
+            more unique.
+          </p>
+          <el-button type="primary">Learn more >></el-button>
+          <br />
+        </el-col>
+      </el-row>
+
+      <el-row type="flex" justify="center">
+        <el-col :span="20" id="second">
+          <el-card shadow="never" v-for="heading in headings" :key="heading">
+            <h2>{{ heading }}</h2>
+            <p>
+              Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
+              tellus ac cursus commodo, tortor mauris condimentum nibh.
+            </p>
+            <el-button size="small">View details >></el-button>
+          </el-card>
+        </el-col>
+      </el-row>
+
+      <el-row type="flex" justify="center">
+        <el-col :span="20" id="copyright">
+          <el-divider></el-divider>
+          <p>© 2020 Company, Inc.</p>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      headings: [
+        "1Heading",
+        "2Heading",
+        "3Heading",
+        "4Heading",
+        "5Heading",
+        "6Heading",
+      ],
+      welcome: "Vue.js,Element-UI",
+      pj_name: "Visualize Knapsack",
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert(
+            "Name:" +
+              this.nameValidateForm.name +
+              ";Password:" +
+              this.nameValidateForm.password
+          );
+          // console.log(this.nameValidateForm.name);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+<style>
+.el-card {
+  background: #fbfbfb;
+  width: 300px;
   display: inline-block;
-  margin: 0 10px;
+  margin: 0.3em;
+  border-radius: 20px;
 }
-a {
-  color: #42b983;
+.grid-content {
+  /* background: rgb(14, 214, 131); */
+  border-radius: 4px;
+  min-height: 80px;
 }
+.text {
+  font-size: 14px;
+}
+.item {
+  padding: 18px 0;
+}
+.header-h2 {
+  color: #fdefd8;
+  font-size: 2em;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.header-h2:hover {
+  color: #fff;
+}
+.row-header {
+  background: #409eff;
+}
+.row-header:hover {
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.08), 0 6px 6px rgba(0, 0, 0, 0.1);
+}
+.content {
+  margin-top: 50px;
+  background: rgb(245, 245, 245);
+}
+.header{
+  box-shadow: 0 5px 20px rgba(0,0,0,.05), 0 6px 6px rgba(0,0,0,.05);
+  position: fixed;
+  z-index: 10;
+  left: 0;
+  right: 0;
+  top: 0;
+  margin-left: 8px;
+  margin-right: 8px;
+}
+#first{
+  background: rgb(255, 255, 255);
+  z-index: 5;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.08), 0 6px 6px rgba(0, 0, 0, 0.1);
+}
+#second{
+  background: rgb(255, 255, 255);
+  z-index: 5;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.08), 0 6px 6px rgba(0, 0, 0, 0.1);
+}
+#copyright{
+  background: rgb(255, 255, 255);
+  z-index: 5;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.08), 0 6px 6px rgba(0, 0, 0, 0.1);
+}
+
 </style>
