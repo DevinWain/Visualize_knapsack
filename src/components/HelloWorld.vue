6eefefbaf10651339c730f0efad4c19ee729215b
DevinWain <1065053260@qq.com>
2020-11-01 00:47:49 +0800

    Implement the computing function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@
 
       <el-row type="flex" justify="center">
         <el-col :span="20" class="second">
-          <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form :inline="true" class="demo-form-inline">
             <el-form-item label="背包大小：">
               <el-input-number
                 v-model="size"
@@ -38,22 +38,20 @@
 
             <el-form-item label="物品体积：" style="margin-left: 20px">
               <el-input-number
-                v-model="formInline.itemsize"
-                @change="handleChange"
+                v-model="item_size"
                 :min="1"
                 :max="100"
               ></el-input-number>
             </el-form-item>
             <el-form-item label="物品价值：">
               <el-input-number
-                v-model="formInline.itemvalue"
-                @change="handleChange"
+                v-model="item_value"
                 :min="1"
                 :max="100"
               ></el-input-number>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="onSubmit">添加</el-button>
+              <el-button type="primary" @click="addItem">添加</el-button>
             </el-form-item>
           </el-form>
         </el-col>
@@ -61,24 +59,27 @@
 
       <el-row type="flex" justify="center">
         <el-col :span="20" class="second">
-          <el-table :data="tableData" style="width: 100%" height="500">
+          <el-table :data="tableData" style="width: 100%" height="500" ref="table">
             <el-table-column
               fixed
-              prop="item"
+              prop="msg"
               label="背包大小"
               width="180"
               align="center"
             >
               <template slot-scope="scope">
-                  <span>体积={{ scope.row.item.size }} 价值={{
-                      scope.row.item.value}}</span>
-                  <el-button
-                    type="danger"
-                    icon="el-icon-delete"
-                    circle
-                    size="mini"
-                    style="margin-left: 10px"
-                  ></el-button>
+                <span
+                  >体积={{scope.row.item.size}}  价值={{
+                    scope.row.item.value
+                  }}</span
+                >
+                <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  circle
+                  size="mini"
+                  style="margin-left: 10px"
+                ></el-button>
               </template>
             </el-table-column>
             <el-table-column
@@ -86,12 +87,13 @@
               prop="dpvalue"
               v-for="(item, index) in parseInt(size) + 1"
               :key="index"
-              :label="(item - 1)+''"
+              :label="item - 1 + ''"
             >
               <template slot-scope="scope">
                 <span>{{ scope.row.dpvalue[index] }}</span>
               </template>
             </el-table-column>
+            
           </el-table>
         </el-col>
       </el-row>
@@ -120,43 +122,103 @@
 </template>
 
 <script>
+var size=101;
 export default {
   data() {
     return {
       headings: ["1Heading", "2Heading", "3Heading", "4Heading"],
       welcome: "Vue.js,Element-UI",
       pj_name: "Visualize Knapsack",
+      item_size: 1,
+      item_value: 1,
+      size: 10,
+      dp: [],
       tableData: [
         {
           item: {
-            value: 4,
-            size: 4,
+            value: 0,
+            size: 0,
           },
-          dpvalue: [1, 2, 3, 4, 5, 6],
+          dpvalue: new Int16Array(size),
         },
-        {
-          item: {
-            value: 5,
-            size: 3,
-          },
-          dpvalue: [2, 2, 3, 4, 5, 6],
-        },
-      ],
-      size: 20,
-      formInline: {
-        bagsize: "",
-        itemsize: "",
-        itemvalue: "",
-      },
+        // {
+        //   item: {
+        //     value: 4,
+        //     size: 4,
+        //   },
+        //   dpvalue: [1, 2, 3, 4, 5, 6],
+        // },
+        // {
+        //   item: {
+        //     value: 5,
+        //     size: 3,
+        //   },
+        //   dpvalue: [2, 2, 3, 4, 5, 6],
+        // }
+        ],
     };
   },
   methods: {
-    handleChange(value) {
-      console.log(value);
+    handleChange() {
+      this.$nextTick(() => {
+        this.$refs.table.doLayout();
+      });
+      this.$options.methods.calculateDp(this);
     },
     onSubmit() {
-        console.log('submit!');
+      console.log("submit!");
     },
+    // 动态规划
+    calculateDp(self) {
+      let weightList = [];
+      weightList[0] = 0;
+      let valueList = [];
+      valueList[0] = 0;
+      for (let i = 1; i <= self.tableData.length; i++) {
+        weightList[i] = parseInt(self.tableData[i - 1].item.size);
+        valueList[i] = parseInt(self.tableData[i - 1].item.value);
+      }
+      self.dp[0] = [];
+      for (let i = 0; i <= self.size; i++) {
+        self.dp[0][i] = 0;
+      }
+      for (let i = 1; i <= self.tableData.length; i++) {
+        self.dp[i] = [];
+        self.dp[i][0] = 0;
+      }
+      for (let i = 1; i <= self.tableData.length; i++) {
+        for (let j = 1; j <= self.size; j++) {
+          if (weightList[i] > j) {
+            self.dp[i][j] = self.dp[i - 1][j];
+          } else {
+            let value1 = self.dp[i - 1][j - weightList[i]] + valueList[i];
+            let value2 = self.dp[i - 1][j];
+            if (value1 > value2) {
+              self.dp[i][j] = value1;
+            } else {
+              self.dp[i][j] = value2;
+            }
+          }
+        }
+      }
+      for (let i = 1; i <= self.tableData.length; i++) {
+            self.tableData[i].dpvalue = self.dp[i+1]
+      }
+    },
+    addItem() {
+          //赋值部分
+          let tempObj = {
+            item: {
+              value: this.item_value,
+              size: this.item_size,
+            },
+            dpvalue: new Int16Array(this.size+1),
+          };
+          this.tableData.push(tempObj);
+          this.item_size = null;
+          this.item_value = null;
+          this.$options.methods.calculateDp(this);
+        },
   },
 };
 </script>
